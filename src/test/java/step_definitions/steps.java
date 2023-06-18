b49d3c5b3a6a24cdb954e3c52b0c1546c5d57570
G-raza778 <raza_pc@raza>
2023-06-18 11:55:36 +0530

    this cucumber is

---
+++ src/test/java/step_definitions/steps.java
@@ -0,0 +1,29 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class steps {
+
+@Given("Launch the Browser")
+public void launch_the_browser() {
+
+}
+
+@When("Insert valid credential user {string} pwd {string}")
+public void insert_valid_credential_user_pwd(String string, String string2) {
+
+}
+
+@Then("Home_page Should Be Displayed")
+public void home_page_should_be_displayed() {
+
+}
+
+@Then("Log_out Application")
+public void log_out_application() {
+
+}
+
+}
---
+++ src/test/resources/feature/Log_in.feature
@@ -0,0 +1,13 @@
+
+Feature: Log_in Home Page
+
+
+
+  Scenario: Title of your scenario
+    Given Launch the Browser
+   
+    When Insert valid credential user "admin" pwd "root"
+
+    Then Home_page Should Be Displayed
+    And Log_out Application
+
